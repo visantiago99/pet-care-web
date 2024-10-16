3c2abaf5ca8b380a78aa3de0199f24da971a41d7
Victor Santiago da Cunha <czpask@gmail.com>
2024-10-15 21:39:20 -0300

    Create pet register form and register pet mutation

--- pet-care-vite/src/App.tsx
+++ pet-care-vite/src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import FormInputField from "./components/formInputField";
 
 interface Pet {
   id: string;
@@ -11,8 +12,17 @@ interface Pet {
   photo: string;
 }
 
+export type RegisterPetForm = Omit<Pet, "id" | "medical_history">;
+
 function App() {
   const [pets, setPets] = useState<Pet[]>([]);
+  const [petRegisterForm, setPetRegisterForm] = useState<RegisterPetForm>({
+    name: "",
+    age: "",
+    breed: "",
+    species: "",
+    photo: "",
+  });
 
   useEffect(() => {
     fetch("http://localhost:3000/pets", {
@@ -22,8 +32,36 @@ function App() {
       .then((data) => setPets(data));
   }, []);
 
+  const handlePetFormOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setPetRegisterForm((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+    console.log(petRegisterForm);
+  };
+
+  const registerPetMutation = (petRegisterForm: RegisterPetForm) => {
+    event?.preventDefault();
+    // const { name, age, breed, species, photo } = petRegisterForm;
+
+    const searchParams = new URLSearchParams();
+    Object.entries(petRegisterForm).forEach((entry) => {
+      if (entry[1]) {
+        searchParams.append(...entry);
+      }
+    });
+
+    fetch("http://localhost:3000/pets", {
+      method: "POST",
+      body: searchParams,
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  };
+
   return (
-    <>
+    <div className="flex flex-col gap-10">
       <div className="flex flex-1 h-full gap-4">
         {pets.map((pet, index) => (
           <div className="border-red-500 border max-w-60" key={index}>
@@ -36,7 +74,47 @@ function App() {
           </div>
         ))}
       </div>
-    </>
+
+      <div className="w-100">
+        <form onSubmit={() => registerPetMutation(petRegisterForm)}>
+          <fieldset className="flex flex-col gap-4">
+            <FormInputField
+              value={petRegisterForm.name}
+              labelValue="Name"
+              name="name"
+              onChange={handlePetFormOnChange}
+            />
+            <FormInputField
+              value={petRegisterForm.age}
+              labelValue="Age"
+              name="age"
+              onChange={handlePetFormOnChange}
+            />
+            <FormInputField
+              value={petRegisterForm.breed}
+              labelValue="Breed"
+              name="breed"
+              onChange={handlePetFormOnChange}
+            />
+            <FormInputField
+              value={petRegisterForm.species}
+              labelValue="Species"
+              name="species"
+              onChange={handlePetFormOnChange}
+            />
+            <FormInputField
+              value={petRegisterForm.photo}
+              labelValue="Photo"
+              name="photo"
+              onChange={handlePetFormOnChange}
+            />
+          </fieldset>
+          <button className="mt-6" type="submit">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
   );
 }
 
