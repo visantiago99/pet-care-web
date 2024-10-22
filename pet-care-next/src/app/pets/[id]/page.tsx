1db75039079d73cb978c4354b6857261886949f2
Victor Santiago da Cunha <czpask@gmail.com>
2024-10-21 21:47:51 -0300

    Implement PetDetails component

--- pet-care-next/src/app/pets/[id]/page.tsx
+++ pet-care-next/src/app/pets/[id]/page.tsx
@@ -3,6 +3,7 @@ import { fetchPetById } from "@/hooks/usePets";
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "next/navigation";
 import { PetData } from "@/schemas/pet";
+import PetDetails from "@/components/PetDetails";
 
 export default function PetDetailsPage() {
   const { id } = useParams();
@@ -20,35 +21,11 @@ export default function PetDetailsPage() {
     enabled: !!id,
   });
 
-  if (isLoading) {
-    return <div>Carregando...</div>;
-  }
-
-  if (error) {
-    return <div>Erro ao carregar os dados do pet</div>;
-  }
-
-  const { name, species, breed, age, medical_history, photo } =
-    petData?.result || {
-      id: "",
-      age: "",
-      breed: "",
-      medical_history: "",
-      name: "",
-      photo: "",
-      species: "",
-    };
-
   return (
-    <div>
-      <h1>Cheguei na página individual do pet!</h1>
-      <p>ID do pet: {id}</p>
-      <p>Nome: {name}</p>
-      <p>Espécie: {species}</p>
-      <p>Raça: {breed}</p>
-      <p>Idade: {age}</p>
-      <p>Histórico Médico: {medical_history}</p>
-      <img src={photo} alt={name} />
-    </div>
+    <>
+      {isLoading && <div>Carregando...</div>}
+      {petData?.result && <PetDetails pet={petData.result} />}
+      {error && <div>Erro ao carregar os dados do pet</div>}
+    </>
   );
 }
---
+++ pet-care-next/src/components/PetDetails.tsx
@@ -0,0 +1,30 @@
+import { PetData } from "@/schemas/pet";
+import React from "react";
+
+interface PetDetailsProps {
+  pet: PetData;
+}
+
+const PetDetails = ({ pet }: PetDetailsProps) => {
+  const { id, age, breed, medical_history, name, photo, species } = pet;
+  return (
+    <div>
+      <div className="p-10 flex gap-4 justify-around">
+        <div>
+          <img src={photo} alt={name} />
+        </div>
+        <div>
+          <h1>Cheguei na página individual de detalhes do pet!</h1>
+          <p>ID do pet: {id}</p>
+          <p>Nome: {name}</p>
+          <p>Espécie: {species}</p>
+          <p>Raça: {breed}</p>
+          <p>Idade: {age}</p>
+          <p>Histórico Médico: {medical_history}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PetDetails;
