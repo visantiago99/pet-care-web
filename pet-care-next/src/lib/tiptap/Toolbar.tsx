42af1571ec97d4c802f11701b4903abf36d22d18
Victor Santiago da Cunha <czpask@gmail.com>
2024-12-10 13:55:22 -0300

    Remove headings from rich text editor

--- pet-care-next/src/lib/tiptap/Toolbar.tsx
+++ pet-care-next/src/lib/tiptap/Toolbar.tsx
@@ -1,14 +1,7 @@
 "use client";
 
 import { type Editor } from "@tiptap/react";
-import {
-  Bold,
-  Strikethrough,
-  Italic,
-  Heading1,
-  Heading2,
-  Heading3,
-} from "lucide-react";
+import { Bold, Strikethrough, Italic } from "lucide-react";
 import { Toggle } from "@/components/ui/toggle";
 
 type Props = {
@@ -22,33 +15,6 @@ const Toolbar = ({ editor }: Props) => {
 
   return (
     <div className="border border-input bg-transparent rounded-md flex space-x-2">
-      <Toggle
-        size={"sm"}
-        pressed={editor.isActive("heading", { level: 1 })}
-        onPressedChange={() =>
-          editor.chain().focus().toggleHeading({ level: 1 }).run()
-        }
-      >
-        <Heading1 className="h-4 w-4" />
-      </Toggle>
-      <Toggle
-        size={"sm"}
-        pressed={editor.isActive("heading", { level: 2 })}
-        onPressedChange={() =>
-          editor.chain().focus().toggleHeading({ level: 2 }).run()
-        }
-      >
-        <Heading2 className="h-4 w-4" />
-      </Toggle>
-      <Toggle
-        size={"sm"}
-        pressed={editor.isActive("heading", { level: 3 })}
-        onPressedChange={() =>
-          editor.chain().focus().toggleHeading({ level: 3 }).run()
-        }
-      >
-        <Heading3 className="h-4 w-4" />
-      </Toggle>
       <Toggle
         size={"sm"}
         pressed={editor.isActive("bold")}
