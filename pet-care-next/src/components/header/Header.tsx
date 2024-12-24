36ef715d29984bbdf2a00935707bd7bdb88bd645
Victor Santiago da Cunha <czpask@gmail.com>
2024-12-23 21:17:02 -0300

    Update Header styles

--- pet-care-next/src/components/header/Header.tsx
+++ pet-care-next/src/components/header/Header.tsx
@@ -2,6 +2,7 @@
 import { Button } from "@/components/ui/button";
 import { useUserContext } from "@/contexts/userContext";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -11,22 +12,46 @@ import {
 
 export default function Header() {
   const { user, logoutUser } = useUserContext();
+  const pathname = usePathname();
+  console.log(pathname);
 
   return (
-    <header className="bg-white shadow-md py-4 px-8 sticky top-0 z-50">
+    <header className="bg-white shadow-md py-4 px-8 sticky top-0 z-50 rounded-b-xl">
       <div className="container mx-auto flex items-center justify-between">
         <div className="text-2xl font-bold">
-          <a href="/">Meu Site</a>
+          <a href="/" className="flex items-center font-poppins">
+            <img
+              src="/icons/pawIcon.svg"
+              alt="Paw Icon"
+              className="h-6 w-6 mr-2"
+            />
+            Adopt Me
+          </a>
         </div>
 
-        <nav className="space-x-6">
-          <a href="/pets" className="text-gray-700 hover:text-gray-900">
+        <nav className="space-x-6 font-poppins">
+          <a
+            href="/pets"
+            className={`link ${
+              pathname === "/pets" ? "link-active" : "link-inactive"
+            }`}
+          >
             Pets
           </a>
-          <a href="/about" className="text-gray-700 hover:text-gray-900">
+          <a
+            href="/about"
+            className={`link ${
+              pathname === "/about" ? "link-active" : "link-inactive"
+            }`}
+          >
             Sobre
           </a>
-          <a href="/contact" className="text-gray-700 hover:text-gray-900">
+          <a
+            href="/contact"
+            className={`link ${
+              pathname === "/contact" ? "link-active" : "link-inactive"
+            }`}
+          >
             Contato
           </a>
         </nav>
@@ -36,24 +61,21 @@ export default function Header() {
             {!user ? (
               <>
                 <Link href={"/login"}>
-                  <Button>Entrar</Button>
+                  <Button className="hover:bg-orange-400">Entrar</Button>
                 </Link>
                 <Link href={"/register"}>
-                  <Button>Cadastrar</Button>
+                  <Button className="hover:bg-orange-400">Cadastrar</Button>
                 </Link>
               </>
             ) : (
               <DropdownMenu>
                 <DropdownMenuTrigger asChild>
                   <div className="text-lg font-medium text-gray-700 cursor-pointer">
-                    {user.username}
+                    {/* Conteúdo do Dropdown */}
                   </div>
                 </DropdownMenuTrigger>
                 <DropdownMenuContent>
-                  <DropdownMenuItem asChild>
-                    <Link href="/account">Minha Conta</Link>
-                  </DropdownMenuItem>
-                  <DropdownMenuItem onClick={logoutUser}>
+                  <DropdownMenuItem onSelect={logoutUser}>
                     Logout
                   </DropdownMenuItem>
                 </DropdownMenuContent>
